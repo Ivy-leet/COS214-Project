70b98dc84526eaa1dfa729ff313e01e08b2cf0d5
Malcolm Dzimati <malcolmdzimati@gmail.com>
2021-11-17 14:29:13 +0200

    Update Subject.h

--- Subject.h
+++ Subject.h
@@ -1,18 +1,21 @@
 #ifndef SUBJECT_H
 #define SUBJECT_H
 
+#include "Observer.h"
 #include <vector>
 #include <iostream>
-#include "Observer.h"
 
-public Subject{
-	private:
-		 vector<Observer> observers = new vector<Observer>();
+using namespace std;
 
-	public:
-		attach(Observer*);
-		detach(Observer*);
-		notify();
+class Subject{
+        private:
+                 vector<Observer*> observers;
+
+        public:
+                void attach(Observer*);
+                void detach(Observer*);
+                void notify();
 };
 
 #endif
+
