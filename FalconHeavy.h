71682b75e8527a2019dd5ae5cdaa9a4fd2731aa0
GkikasV <75447279+GkikasV@users.noreply.github.com>
2021-11-22 15:16:26 +0200

    Create FalconHeavy.h

--- FalconHeavy.h
+++ FalconHeavy.h
@@ -2,13 +2,14 @@
 #define FALCONHEAVY_H
 #include "Rocket.h"
 #include <list>
+#include <vector>
+
+class Rocket;
 
 /**
  * @class FalconHeavy "FalconHeavy.h"
- * @brief The composite participant in the Composite design pattern. Holds pointers to FalconNine objects
-*/
-
-class Rocket;
+ * @brief A class which implements the Rocket's specification. This class is the ConcreteClass participant in the Template Method DP
+ */
 
 class FalconHeavy : public Rocket {
     private:
@@ -18,10 +19,36 @@ class FalconHeavy : public Rocket {
         FalconHeavy();
         ~FalconHeavy();
         void rocketInfo() override;
+    
+ /**
+ * @brief Fires the Stage Rockets
+ * @return void
+ */
         virtual void fire();
+    
+ /**
+ * @brief Detaches the Stage Rockets
+ * @return State pointer
+ */
         virtual void detach();
+    
+ /**
+ * @brief Docks the Stage Rockets
+ * @return void
+ */
         virtual void dock();
+    
+ /**
+ * @brief Adds a FalconNine Rocket
+ * @param r - Rocket*
+ * @return void
+ */
         virtual void addFalconNine(Rocket*);
+    
+ /**
+ * @brief Removes a FalconNine Rocket
+ * @return Rocket pointer
+ */
         virtual Rocket* removeFalconNine();
 };
 
