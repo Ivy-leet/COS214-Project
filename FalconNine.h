800619782c8078e1c3d486e88764988a26bcf578
GkikasV <75447279+GkikasV@users.noreply.github.com>
2021-11-22 15:20:20 +0200

    Update FalconNine.h

--- FalconNine.h
+++ FalconNine.h
@@ -3,14 +3,31 @@
 #include "Rocket.h"
 
 /**
- * @class FalconNine "FalconNine"
-*/
+ * @class FalconNine "FalconNine.h"
+ * @brief A class which implements the Rocket's specification. This class is the ConcreteClass participant in the Template Method DP
+ */
 
 class FalconNine : public Rocket {
     public:
         FalconNine();
+    
+ /**
+ * @brief Displays information about the Rocket
+ * @return void
+ */
         void rocketInfo() override;
+    
+ /**
+ * @brief Adds a FalconNine Rocket
+ * @param r - Rocket*
+ * @return void
+ */
         virtual void addFalconNine(Rocket*);
+    
+/**
+ * @brief Removes a FalconNine Rocket
+ * @return Rocket pointer
+ */
         virtual Rocket* removeFalconNine();
 };
 
