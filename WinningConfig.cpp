72c719ed27b75a6e0604765c148eec2c737b0505
GkikasV <75447279+GkikasV@users.noreply.github.com>
2021-11-22 11:18:09 +0200

    Update WinningConfig.cpp

--- WinningConfig.cpp
+++ WinningConfig.cpp
@@ -10,13 +10,26 @@ WinningConfig::~WinningConfig()
 	bestShuttle=nullptr;
 }
 
+/**
+ * @brief Returns the stored Memento(WinningShuttle)
+ * @param None
+ * @return WinningShuttle pointer
+ */
+
 WinningShuttle* WinningConfig::retrieveWinningShuttle()
 {
 	return bestShuttle;
 }
 
+/**
+ * @brief Stores the Memento(WinningShuttle)
+ * @param ws - WinningShuttle* 
+ * @return void
+ */
+
 void WinningConfig::storeWinningShuttle(WinningShuttle* ws)
 {
-	delete bestShuttle;
+	if(bestShuttle != nullptr)
+		delete bestShuttle;
 	bestShuttle = ws;
 }
