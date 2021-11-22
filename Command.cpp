156d0b77f163774855c227b4e6d28d1a04211858
Ivy-leet <tlholokoma@gmail.com>
2021-11-22 16:51:14 +0200

    Chain of Responsibility and Prototype

--- Command.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Command.h"
-using namespace std;
-
-Command::Command(Rocket* r)
-{
-    rocket = r;
-}
--- Starlink.h
+++ Starlink.h
@@ -1,13 +1,20 @@
-#ifndef STARLINK_H
-#define STARLINK_H
+#include "Satellite.h"
+#include "Handler.h"
 
-/**
- * @class Starlink "Starlink.h"
- * @brief A class for creating the starlinks
- */
-class Starlink {
-    public:
-        Starlink *clone();
+class Starlink : public Satellite, Handler //ConcretePrototype
+{
+
+static int StarLinkIDInc ; //to give each starlink satellite a unique identifictation. Incremented whenever new object made
+
+private:
+    /* data */
+    int StarlinkID = 0;
+    Handler *successor; //the next object to handle request
+public:
+    Starlink* clone();
+    void setNextHandler(Handler *nextH); //If the current object cannot handle the request, send it to the next
+    void HandleRequest(Message inMes); //if the message can be handled, notify client
+    Starlink(/* args */);
+    Starlink(const Starlink& StarL);
+    ~Starlink();
 };
-
-#endif
