2fcd5c79899a921e2d7151f9c62b3aa384117a33
GkikasV <75447279+GkikasV@users.noreply.github.com>
2021-11-22 11:55:35 +0200

    Create WinningShuttle.cpp

--- WinningShuttle.cpp
+++ WinningShuttle.cpp
@@ -10,21 +10,9 @@ WinningShuttle::~WinningShuttle()
 	spaceShuttle=nullptr;
 }
 
-/**
- * @brief Stores the WinningShuttle(SpaceShuttle)
- * @param s - SpaceShuttle*
- * @return void
- */
-
 void WinningShuttle::setWinningShuttle(SpaceShuttle* s) {
 	spaceShuttle=s;
 }
-
-/**
- * @brief Returns the WinningShuttle(SpaceShuttle)
- * @param None
- * @return SpaceShuttle pointer
- */
 
 SpaceShuttle* WinningShuttle::getWinningShuttle() {
 	return spaceShuttle;
