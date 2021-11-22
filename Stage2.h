6f07c15e8d7f8e1e0b1b8fbed38188f707c88dfe
GkikasV <75447279+GkikasV@users.noreply.github.com>
2021-11-22 12:18:34 +0200

    Update Stage2.h

--- Stage2.h
+++ Stage2.h
@@ -6,14 +6,40 @@
 #include "State.h"
 using namespace std;
 
+/**
+ * @class Stage2 "Stage2.h"
+ * @brief A class which implements the behaviour of the Engine rockets. This class is the ConcreteState participant in the State DP
+ */
+
 class Stage2: public State
 {
   private:
+  
+ /**
+ * @brief Returns a nullptr object
+ * @return void
+ */
     State* changeState();
+    
   public:
     Stage2(string msg);
+    
+ /**
+ * @brief Fires the Stage Rockets
+ * @return void
+ */
     void fire();
+    
+ /**
+ * @brief Docks the Stage Rockets
+ * @return void
+ */
     void dock();
+    
+ /**
+ * @brief Detaches the Stage Rockets
+ * @return State pointer
+ */
     State* detach();
 };
 #endif
